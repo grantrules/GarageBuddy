3743e2f04637cea5133a94f3f67e6ec40b9296f9
grantrules <gharding@gmail.com>
2024-04-30 03:02:36 -0400

    need to add context

--- backend/go.mod
+++ backend/go.mod
@@ -4,6 +4,11 @@ go 1.18
 
 require (
 	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
+	github.com/jackc/pgx/v5 v5.5.5 // indirect
+	github.com/jackc/puddle/v2 v2.2.1 // indirect
+	golang.org/x/sync v0.1.0 // indirect
 	golang.org/x/time v0.5.0 // indirect
 )
 
--- backend/hello.go
+++ backend/hello.go
@@ -2,13 +2,18 @@ package main
 
 import (
 	"database/sql"
+	"errors"
 	"log"
 	"net/http"
+	"strings"
 
+	_ "github.com/jackc/pgx/v5/stdlib"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
-	_ "github.com/lib/pq"
+	"golang.org/x/crypto/scrypt"
 )
+
+var db *sql.DB
 
 type Login struct {
 	Email    string `json:"email" xml:"email" form:"email" query:"email"`
@@ -22,15 +27,101 @@ type Register struct {
 	PasswordConfirm string `json:"password-confirm" xml:"password-confirm" form:"password-confirm" query:"password-confirm"`
 }
 
+type User struct {
+	ID       string
+	Name     string
+	Email    string
+	Password string
+}
+
+func createUser(u User) (int64, error) {
+	result, err := db.Exec("INSERT user VALUES (null, ?, ?, ?)", u.Name, u.Email, u.Password)
+	if err != nil {
+		log.Print(err)
+		return -1, err
+	}
+	id, err := result.LastInsertId()
+	if err != nil {
+		log.Print(err)
+	}
+	return id, err
+}
+
+func getUserByEmail(email string) (User, error) {
+	var u User
+	row := db.QueryRow("SELECT * FROM user WHERE Email = ?", email)
+	if err := row.Scan(&u.ID, &u.Name, &u.Email, &u.Password); err != nil {
+		return u, errors.New("unable to find user by email")
+	}
+	return u, nil
+
+}
+
+func hashPassword(password string) (string, error) {
+	salt := []byte("poopoo peepee")
+
+	hashedPass, err := scrypt.Key([]byte(password), salt, 32768, 8, 1, 32)
+
+	return string(hashedPass[:]), err
+}
+
+func loginUser(l Login) (User, error) {
+	u, err := getUserByEmail(l.Email)
+	if err != nil {
+		return u, errors.New("login failed")
+	}
+
+	hashedPassword, err := hashPassword(l.Password)
+	if err != nil {
+		return u, errors.New("login failed")
+	}
+
+	if u.Password != hashedPassword {
+		return u, errors.New("login failed")
+	}
+
+	return u, nil
+
+}
+
+func registerUser(r Register) (int64, error) {
+	if r.Name == "" {
+		return -1, errors.New("name cannot be empty")
+	}
+	if !strings.Contains(r.Email, "@") {
+		return -1, errors.New("invalid email")
+	}
+	if r.Password != r.PasswordConfirm {
+		return -1, errors.New("passwords don't match")
+
+	}
+	hashedPass, err := hashPassword(r.Password)
+	if err != nil {
+		return -1, errors.New("error")
+	}
+
+	u := new(User)
+	u.Name = r.Name
+	u.Email = r.Email
+	u.Password = hashedPass
+
+	return createUser(*u)
+}
+
 func hello(c echo.Context) error {
 	return c.String(http.StatusOK, "Hello, WorldPoop!")
 }
 
 func login(c echo.Context) error {
-	u := new(Login)
-	if err := c.Bind(u); err != nil {
+	var l Login
+	if err := c.Bind(l); err != nil {
 		return err
 	}
+	u, err := loginUser(l)
+	if err != nil {
+		return c.JSON(http.StatusForbidden, "Login failed")
+	}
+	u.Password = ""
 	return c.JSON(http.StatusCreated, u)
 }
 
@@ -43,7 +134,14 @@ func register(c echo.Context) error {
 	if err := c.Bind(r); err != nil {
 		return err
 	}
-	return c.JSON(http.StatusCreated, r)
+	userId, err := registerUser(*r)
+	if err != nil {
+		log.Print(err)
+
+		return c.JSON(http.StatusNotAcceptable, err)
+	} else {
+		return c.JSON(http.StatusCreated, userId)
+	}
 }
 
 func resetPass(c echo.Context) error {
@@ -51,9 +149,8 @@ func resetPass(c echo.Context) error {
 }
 
 func main() {
-
 	connStr := "postgres://carmaint:example@postgres/carmaint"
-	db, err := sql.Open("postgres", connStr)
+	db, err := sql.Open("pgx", connStr)
 	if err != nil {
 		log.Fatal(err)
 	}
